773c9be83a118590cdda4f1885be7138864efcc4
justapig9020 <justapig9020@gmail.com>
2022-12-16 16:26:17 +0800

    Add command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod traffic_gen;
 
 use std::collections::HashMap;
 
+use clap::Parser;
 use report::Report;
 use scheduler::Scheduler;
 use scheduler::DP;
@@ -12,21 +13,35 @@ use traffic_gen::Traffic;
 use crate::scheduler::FCFS;
 use crate::traffic_gen::Parameters;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    n: usize,
+    #[arg(short, long)]
+    lambda: f32,
+    #[arg(short, long, default_value_t = 1)]
+    times: usize,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let para = Parameters::default();
-
     let schedulers: Vec<&dyn Scheduler> = vec![&DP, &FCFS];
+    let mut report = Vec::new();
 
-    for l in 1..=1 {
-        let lambda = l as f32 * 0.1;
-        let count = 100;
+    let count = args.n;
+    let lambda = args.lambda;
+    let times = args.times;
+    for _ in 0..times {
         let traffic = Traffic::generate(lambda, count);
         let mut schedules = HashMap::new();
         for schd in schedulers.iter() {
             let sol = schd.solve(&traffic, &para).unwrap();
             schedules.insert(schd.method(), sol);
         }
-        let report = Report::new(&traffic, &schedules);
-        println!("{}", serde_json::to_string(&report).unwrap());
+        report.push(Report::new(&traffic, &schedules));
     }
+    println!("{}", serde_json::to_string(&report).unwrap());
 }
