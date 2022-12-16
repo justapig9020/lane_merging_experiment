b1a97f8a17b9f74c3b93feb55bceef30db846a48
justapig9020 <justapig9020@gmail.com>
2022-12-16 10:42:14 +0800

    Avoid uninitialize bug in DP scheduling

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use traffic_gen::Traffic;
 use crate::traffic_gen::Parameters;
 
 fn main() {
-    let lamdba = 1.0;
-    let count = 5;
+    let lamdba = 0.1;
+    let count = 100;
     let para = Parameters::default();
     let traffic = Traffic::generate(lamdba, count);
     println!("{traffic:?}");
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -16,12 +16,12 @@ pub trait Scheduler {
 #[derive(Debug)]
 pub struct Schedule {
     scheduled_entering_times: Vec<Lane>,
-    total_time: Duration,
+    t_last: Duration,
 }
 
 impl Schedule {
     pub fn total_time(&self) -> Duration {
-        self.total_time
+        self.t_last
     }
     pub fn scheduled_entering_times(&self) -> Vec<&[Duration]> {
         self.scheduled_entering_times
@@ -37,13 +37,12 @@ impl Scheduler for DP {
     fn solve(traffic: &Traffic, para: &Parameters) -> Option<Schedule> {
         let eats = traffic.earlist_arrival_times();
         let a = eats.get(0)?;
-        println!("{a:?}");
         let b = eats.get(1)?;
-        println!("{b:?}");
         let alpha = a.len();
         let beta = b.len();
         let w_e = para.w_e; // W= in paper
         let w_p = para.w_p; // W+ in paper
+        let inf = Duration::MAX - w_p;
 
         // l(i, j, A) => i and j vehicle in passed vehicles are from A lane and B lane, the last vehicle passed the merging point is from A lane
         let mut l = vec![vec![vec![Duration::default(); 2]; beta + 1]; alpha + 1];
@@ -55,17 +54,21 @@ impl Scheduler for DP {
         l[0][0][B] = Duration::from_secs(0);
 
         l[1][0][A] = a[0];
+        l[1][0][B] = inf;
         l[0][1][B] = b[0];
+        l[0][1][A] = inf;
 
         // If all of the passed vehicles are from A lane
         for i in 2..=alpha {
             l[i][0][A] = Duration::max(a[i - 1], l[i - 1][0][A] + w_e);
+            l[i][0][B] = inf;
             prev[i][0][A] = A;
         }
 
         // If all of the passed vehicles are from B lane
         for j in 2..=alpha {
             l[0][j][B] = Duration::max(b[j - 1], l[0][j - 1][B] + w_e);
+            l[0][j][A] = inf;
             prev[0][j][B] = B;
         }
 
@@ -85,17 +88,9 @@ impl Scheduler for DP {
                 println!("w: {:?}", w_e);
                 println!("l + w: {:?}", l[i - 1][j][A] + w_e);
                  */
-                let prev_from_a = if i - 1 == 0 {
-                    Duration::MAX
-                } else {
-                    Duration::max(a[i - 1], l[i - 1][j][A] + w_e)
-                };
+                let prev_from_a = Duration::max(a[i - 1], l[i - 1][j][A] + w_e);
                 //println!("prev: {:?}", prev_from_a);
-                let prev_from_b = if j == 0 {
-                    Duration::MAX
-                } else {
-                    Duration::max(a[i - 1], l[i - 1][j][B] + w_p)
-                };
+                let prev_from_b = Duration::max(a[i - 1], l[i - 1][j][B] + w_p);
                 (l[i][j][A], prev[i][j][A]) = if prev_from_a <= prev_from_b {
                     (prev_from_a, A)
                 } else {
@@ -103,16 +98,8 @@ impl Scheduler for DP {
                 };
 
                 // The passing vehicle is from lane B
-                let prev_from_a = if i == 0 {
-                    Duration::MAX
-                } else {
-                    Duration::max(b[j - 1], l[i][j - 1][A] + w_p)
-                };
-                let prev_from_b = if j - 1 == 0 {
-                    Duration::MAX
-                } else {
-                    Duration::max(b[j - 1], l[i][j - 1][B] + w_e)
-                };
+                let prev_from_a = Duration::max(b[j - 1], l[i][j - 1][A] + w_p);
+                let prev_from_b = Duration::max(b[j - 1], l[i][j - 1][B] + w_e);
 
                 (l[i][j][B], prev[i][j][B]) = if prev_from_a <= prev_from_b {
                     (prev_from_a, A)
@@ -121,31 +108,8 @@ impl Scheduler for DP {
                 };
             }
         }
-        for k in l.iter() {
-            for p in k.iter() {
-                print!("{:?} ", p[0]);
-            }
-            println!("");
-        }
-        for k in l.iter() {
-            for p in k.iter() {
-                print!("{:?} ", p[1]);
-            }
-            println!("");
-        }
-        for k in prev.iter() {
-            for p in k.iter() {
-                print!("{:?} ", p[0]);
-            }
-            println!("");
-        }
-        for k in prev.iter() {
-            for p in k.iter() {
-                print!("{:?} ", p[1]);
-            }
-            println!("");
-        }
-        let (total_time, last_from) = if l[alpha][beta][A] <= l[alpha][beta][B] {
+
+        let (t_last, last_from) = if l[alpha][beta][A] <= l[alpha][beta][B] {
             (l[alpha][beta][A], A)
         } else {
             (l[alpha][beta][B], B)
@@ -158,12 +122,13 @@ impl Scheduler for DP {
         let mut i = alpha;
         let mut j = beta;
         let mut passing_from = last_from;
-        while i > 0 && j > 0 {
+        while i > 0 || j > 0 {
             let set = l[i][j][passing_from];
+            let prev_from = prev[i][j][passing_from];
             let idx = if passing_from == A { &mut i } else { &mut j };
             scheduled_entering_times[passing_from][*idx - 1] = set;
             *idx -= 1;
-            passing_from = prev[i][j][passing_from];
+            passing_from = prev_from;
         }
         let scheduled_entering_times = scheduled_entering_times
             .into_iter()
@@ -171,7 +136,7 @@ impl Scheduler for DP {
             .collect_vec();
         Some(Schedule {
             scheduled_entering_times,
-            total_time,
+            t_last,
         })
     }
 }
